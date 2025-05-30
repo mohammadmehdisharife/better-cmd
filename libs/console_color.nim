4f63079931614705a06bd959d6749b8170833b6a
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-05-30 01:52:02 -0700

    add config.nims & console_color library

---
+++ libs/console_color.nim
@@ -0,0 +1,19 @@
+const
+  # ACSII 4-Bit colors   
+  BoldBlack       = "\x1b[1;30m"
+  BoldRed         = "\x1b[1;31m"
+  BoldGreen       = "\x1b[1;32m"
+  BoldYellow      = "\x1b[1;33m"
+  BoldBlue        = "\x1b[1;34m"
+  BoldMagenta     = "\x1b[1;35m"
+  BoldCyan        = "\x1b[1;36m"
+  BoldWhite       = "\x1b[1;37m"
+  # ACSII 8-Bit colors   
+  BoldOrange      = "\x1b[1;38;5;208m"
+  BoldPink        = "\x1b[1;38;5;205m"
+  BoldLightBlue   = "\x1b[1;38;5;117m"
+  BoldLightGreen  = "\x1b[1;38;5;119m"
+  BoldPurple      = "\x1b[1;38;5;135m"
+  BoldTurquoise   = "\x1b[1;38;5;80m"
+  BoldGold        = "\x1b[1;38;5;220m"
+  BoldCoral       = "\x1b[1;38;5;203m"
