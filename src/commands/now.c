5e32f0b77aff157ec873a15fa9f6dc9d7dbe4511
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-06-06 01:11:31 -0700

    best performace for windows -> now.c

--- src/commands/now.c
+++ src/commands/now.c
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
-#ifdef _WIN32
 #include <windows.h>
-#endif
 
 #define DEFAULT_FORMAT "%Y-%m-%d %H:%M:%S"
 #define MAX_FORMAT_LEN 100
@@ -14,47 +11,26 @@
 #define COLOR_RED          "\x1b[1;31m"
 #define COLOR_GREEN        "\x1b[1;32m"
 #define ANSI_BOLD_YELLOW   "\x1b[1;33m"
-#define COLOR_CYAN         "\x1b[1;36m"
 #define COLOR_RESET        "\x1b[0m"
 
 int getFormattedTime(char *buffer, size_t bufsize, const char *format, int useUTC, time_t *customTime) {
     struct tm timeinfo;
-    time_t rawtime;
-
-    if (customTime) {
-        rawtime = *customTime;
-    } else {
-        time(&rawtime);
-    }
+    time_t rawtime = customTime ? *customTime : time(NULL);
 
     if (useUTC) {
-#ifdef _WIN32
         if (gmtime_s(&timeinfo, &rawtime) != 0) return -1;
-#else
-        struct tm *tmp = gmtime(&rawtime);
-        if (!tmp) return -1;
-        timeinfo = *tmp;
-#endif
     } else {
-#ifdef _WIN32
         if (localtime_s(&timeinfo, &rawtime) != 0) return -1;
-#else
-        struct tm *tmp = localtime(&rawtime);
-        if (!tmp) return -1;
-        timeinfo = *tmp;
-#endif
     }
 
     if (strftime(buffer, bufsize, format, &timeinfo) == 0) return -1;
-
     return 0;
 }
 
 int parseISO8601(const char *isoStr, time_t *outTime) {
     struct tm timeinfo = {0};
-
-#ifdef _WIN32
     int year, month, day, hour, min, sec;
+
     if (sscanf(isoStr, "%4d-%2d-%2dT%2d:%2d:%2d", &year, &month, &day, &hour, &min, &sec) == 6) {
         timeinfo.tm_year = year - 1900;
         timeinfo.tm_mon = month - 1;
@@ -62,28 +38,13 @@ int parseISO8601(const char *isoStr, time_t *outTime) {
         timeinfo.tm_hour = hour;
         timeinfo.tm_min = min;
         timeinfo.tm_sec = sec;
-        if (strchr(isoStr, 'Z')) {
-            *outTime = _mkgmtime(&timeinfo);
-        } else {
-            *outTime = mktime(&timeinfo);
-        }
+
+        *outTime = strchr(isoStr, 'Z') ? _mkgmtime(&timeinfo) : mktime(&timeinfo);
         return 0;
     }
     return -1;
-#else
-    char *res = strptime(isoStr, "%Y-%m-%dT%H:%M:%S", &timeinfo);
-    if (!res) return -1;
-
-    if (*res == 'Z') {
-        *outTime = timegm(&timeinfo);
-    } else {
-        *outTime = mktime(&timeinfo);
-    }
-    return 0;
-#endif
 }
 
-#ifdef _WIN32
 int getMilliseconds() {
     FILETIME ft;
     ULARGE_INTEGER uli;
@@ -93,33 +54,21 @@ int getMilliseconds() {
     ULONGLONG ms = (uli.QuadPart / 10000) % 1000;
     return (int)ms;
 }
-#else
-int getMilliseconds() {
-    struct timespec ts;
-    if (clock_gettime(CLOCK_REALTIME, &ts) == 0) {
-        return (int)(ts.tv_nsec / 1000000);
-    }
-    return 0;
-}
-#endif
 
 void printUsage(const char *progName) {
-    printf(ANSI_BOLD_YELLOW"Usage: %s [-u] [-f FORMAT] [-t TIMESTAMP] [-I ISO8601] [-i] [-m] [--json] [-h|--help]\n", progName);
-    printf("  -u           Print time in UTC (default local time)"  "\n");
+    printf(ANSI_BOLD_YELLOW "Usage: %s [-u] [-f FORMAT] [-t TIMESTAMP] [-I ISO8601] [-i] [-m] [-j|--json] [-h|--help]\n", progName);
+    printf("  -u           Print time in UTC (default local time)\n");
     printf("  -f FORMAT    Specify strftime format string (default: \"%s\")\n", DEFAULT_FORMAT);
     printf("  -t TIMESTAMP Print time for given UNIX timestamp (seconds since epoch)\n");
     printf("  -I ISO8601   Parse ISO8601 time string (e.g. 2025-06-05T14:30:00Z)\n");
-    printf("  -i           Output time in ISO8601 format"  "\n");
+    printf("  -i           Output time in ISO8601 format\n");
     printf("  -m           Print milliseconds (only with current time)\n");
     printf("  --json       Output result in JSON format\n");
     printf("  -h, --help   Show this help message\n" COLOR_RESET);
 }
 
 int main(int argc, char *argv[]) {
-    int useUTC = 0;
-    int outputISO = 0;
-    int printMillis = 0;
-    int outputJSON = 0;
+    int useUTC = 0, outputISO = 0, printMillis = 0, outputJSON = 0;
     char format[MAX_FORMAT_LEN] = DEFAULT_FORMAT;
     char timeBuffer[MAX_BUFFER_SIZE];
     time_t customTimeValue = 0;
@@ -129,36 +78,15 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-u") == 0) {
             useUTC = 1;
-        } else if (strcmp(argv[i], "-f") == 0) {
-            if (i + 1 < argc) {
-                strncpy(format, argv[i + 1], MAX_FORMAT_LEN - 1);
-                format[MAX_FORMAT_LEN - 1] = '\0';
-                outputISO = 0;
-                i++;
-            } else {
-                fprintf(stderr, COLOR_RED "Error: -f option requires a format string\n" COLOR_RESET);
-                printUsage(argv[0]);
-                return 1;
-            }
-        } else if (strcmp(argv[i], "-t") == 0) {
-            if (i + 1 < argc) {
-                customTimeValue = (time_t)atoll(argv[i + 1]);
-                customTimePtr = &customTimeValue;
-                i++;
-            } else {
-                fprintf(stderr, COLOR_RED "Error: -t option requires a timestamp\n" COLOR_RESET);
-                printUsage(argv[0]);
-                return 1;
-            }
-        } else if (strcmp(argv[i], "-I") == 0) {
-            if (i + 1 < argc) {
-                isoInputStr = argv[i + 1];
-                i++;
-            } else {
-                fprintf(stderr, COLOR_RED "Error: -I option requires an ISO8601 string\n" COLOR_RESET);
-                printUsage(argv[0]);
-                return 1;
-            }
+        } else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+            strncpy(format, argv[++i], MAX_FORMAT_LEN - 1);
+            format[MAX_FORMAT_LEN - 1] = '\0';
+            outputISO = 0;
+        } else if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) {
+            customTimeValue = (time_t)_atoi64(argv[++i]);
+            customTimePtr = &customTimeValue;
+        } else if (strcmp(argv[i], "-I") == 0 && i + 1 < argc) {
+            isoInputStr = argv[++i];
         } else if (strcmp(argv[i], "-i") == 0) {
             outputISO = 1;
         } else if (strcmp(argv[i], "-m") == 0) {
@@ -183,34 +111,20 @@ int main(int argc, char *argv[]) {
         customTimePtr = &customTimeValue;
     }
 
-    if (outputISO) {
-        const char *isoFormat = useUTC ? "%Y-%m-%dT%H:%M:%SZ" : "%Y-%m-%dT%H:%M:%S%z";
-        if (getFormattedTime(timeBuffer, sizeof(timeBuffer), isoFormat, useUTC, customTimePtr) != 0) {
-            fprintf(stderr, COLOR_RED "Failed to format time in ISO8601\n" COLOR_RESET);
-            return 1;
-        }
-    } else {
-        if (getFormattedTime(timeBuffer, sizeof(timeBuffer), format, useUTC, customTimePtr) != 0) {
-            fprintf(stderr, COLOR_RED "Failed to get formatted time\n" COLOR_RESET);
-            return 1;
-        }
+    const char *fmt = outputISO ? (useUTC ? "%Y-%m-%dT%H:%M:%SZ" : "%Y-%m-%dT%H:%M:%S") : format;
+    if (getFormattedTime(timeBuffer, sizeof(timeBuffer), fmt, useUTC, customTimePtr) != 0) {
+        fprintf(stderr, COLOR_RED "Failed to format time\n" COLOR_RESET);
+        return 1;
     }
 
     if (outputJSON) {
-        int ms = 0;
-        if (printMillis && !customTimePtr) {
-            ms = getMilliseconds();
-        }
-        printf("{\n");
-        printf("  \"time\": \"%s\"", timeBuffer);
-        if (printMillis && !customTimePtr) {
-            printf(",\n  \"milliseconds\": %d", ms);
-        }
+        int ms = (printMillis && !customTimePtr) ? getMilliseconds() : 0;
+        printf("{\n  \"time\": \"%s\"", timeBuffer);
+        if (printMillis && !customTimePtr) printf(",\n  \"milliseconds\": %d", ms);
         printf("\n}\n");
     } else {
         if (printMillis && !customTimePtr) {
-            int ms = getMilliseconds();
-            printf(COLOR_GREEN "%s.%03d\n" COLOR_RESET, timeBuffer, ms);
+            printf(COLOR_GREEN "%s.%03d\n" COLOR_RESET, timeBuffer, getMilliseconds());
         } else {
             printf(COLOR_GREEN "%s\n" COLOR_RESET, timeBuffer);
         }
