b8d02f7c1c62dda236eddced3772aa10403725e1
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-06-08 02:45:52 -0700

    minimal cat.c touch.c

--- src/commands/cat.c
+++ src/commands/cat.c
@@ -7,172 +7,57 @@
 #define COLOR_YELLOW    "\x1b[1;33m"
 #define COLOR_RESET     "\x1b[0m"
 
-int showLineNumbers = 0;
-int numberNonBlank = 0;
-int squeezeBlank = 0;
-int showEnds = 0;
-int showTabs = 0;
-
-int isBlankLine(const char *line) {
-	for (const char *p = line; *p; ++p) {
-		if (*p != ' ' && *p != '\t' && *p != '\n' && *p != '\r') {
-			return 0;
-		}
-	}
-	return 1;
-}
-
-void printLine(const char *line, int *lineNumber, int *lastBlank) {
-	int blank = isBlankLine(line);
-
-	if (squeezeBlank && blank && *lastBlank) {
-		return;
-	}
-
-	if (showLineNumbers && !(numberNonBlank && blank)) {
-		printf("%6d\t", (*lineNumber)++);
-	}
-
-	*lastBlank = blank;
-
-	for (const char *p = line; *p; ++p) {
-		if (*p == '\t' && showTabs) {
-			fputs("^I", stdout);
-		} else if (*p == '\n' && showEnds) {
-			putchar('$');
-			putchar('\n');
-		} else {
-			putchar(*p);
-		}
-	}
-}
-
 void printFile(const char *filename) {
-	FILE *file = fopen(filename, "r");
-	if (!file) {
-		fprintf(stderr, COLOR_BOLD_RED "cat: cannot open '%s': %s" COLOR_RESET "\n", filename, strerror(errno));
-		return;
-	}
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, COLOR_BOLD_RED "cat: cannot open '%s': %s" COLOR_RESET "\n", filename, strerror(errno));
+        return;
+    }
 
-	char buffer[4096];
-	int lineNumber = 1;
-	int lastBlank = 0;
+    char buffer[4096];
+    while (fgets(buffer, sizeof(buffer), file)) {
+        fputs(buffer, stdout);
+    }
 
-	while (fgets(buffer, sizeof(buffer), file)) {
-		size_t len = strlen(buffer);
-		int lineComplete = (len > 0 && buffer[len - 1] == '\n');
-
-		if (lineComplete) {
-			printLine(buffer, &lineNumber, &lastBlank);
-		} else {
-			char *lineBuffer = malloc(len + 1);
-			if (!lineBuffer) {
-				fprintf(stderr, COLOR_BOLD_RED "cat: memory allocation error\n" COLOR_RESET);
-				fclose(file);
-				return;
-			}
-			memcpy(lineBuffer, buffer, len);
-			size_t currentLen = len;
-
-			while (!lineComplete) {
-				if (!fgets(buffer, sizeof(buffer), file)) {
-					break;
-				}
-				len = strlen(buffer);
-				lineComplete = (len > 0 && buffer[len - 1] == '\n');
-
-				char *newBuffer = realloc(lineBuffer, currentLen + len + 1);
-				if (!newBuffer) {
-					free(lineBuffer);
-					fprintf(stderr, COLOR_BOLD_RED "cat: memory allocation error\n" COLOR_RESET);
-					fclose(file);
-					return;
-				}
-				lineBuffer = newBuffer;
-
-				memcpy(lineBuffer + currentLen, buffer, len);
-				currentLen += len;
-				lineBuffer[currentLen] = '\0';
-			}
-
-			printLine(lineBuffer, &lineNumber, &lastBlank);
-			free(lineBuffer);
-		}
-	}
-
-	fclose(file);
-}
-
-void printUsage() {
-	fprintf(stderr, COLOR_BOLD_RED
-		"Usage: cat [options] file...\n"
-		"Try 'cat -h' for more information.\n"
-		COLOR_RESET);
+    fclose(file);
 }
 
 void printHelp() {
-	printf(COLOR_YELLOW
-		"Usage: cat [options] file...\n"
-		"Options:\n"
-		"  -n    Number all lines\n"
-		"  -b    Number non-empty lines\n"
-		"  -s    Squeeze blank lines\n"
-		"  -E    Show $ at end of line\n"
-		"  -T    Show tabs as ^I\n"
-		"  -h, --help    Show this help message\n"
-		COLOR_RESET);
+    printf(COLOR_YELLOW
+        "Usage: cat [file...]\n"
+        "\n"
+        "Concatenate and display the content of one or more files.\n"
+        "Examples:\n"
+        "  cat file.txt        # print a single file\n"
+        "  cat a.txt b.txt     # print multiple files in order\n"
+        "\n"
+        "Options:\n"
+        "  -h, --help          Show this help message\n"
+        COLOR_RESET);
+}
+
+void printUsage() {
+    fprintf(stderr, COLOR_BOLD_RED
+        "Usage: cat [file...]\n"
+        "Try 'cat -h' for more information.\n"
+        COLOR_RESET);
 }
 
 int main(int argc, char *argv[]) {
-	if (argc == 1) {
-		fprintf(stderr, COLOR_BOLD_RED "cat: missing file operand\n" COLOR_RESET);
-		printUsage();
-		return 1;
-	}
+    if (argc == 1) {
+        fprintf(stderr, COLOR_BOLD_RED "cat: missing file operand\n" COLOR_RESET);
+        printUsage();
+        return 1;
+    }
 
-	int firstFileIndex = argc;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            printHelp();
+            return 0;
+        } else {
+            printFile(argv[i]);
+        }
+    }
 
-	for (int i = 1; i < argc; i++) {
-		if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-			printHelp();
-			return 0;
-		}
-
-		if (argv[i][0] == '-') {
-			const char *opt = argv[i] + 1;
-			if (*opt == '\0') {
-				fprintf(stderr, COLOR_BOLD_RED "cat: '-' is not supported as input\n" COLOR_RESET);
-				return 1;
-			}
-			while (*opt) {
-				switch (*opt) {
-					case 'n': showLineNumbers = 1; break;
-					case 'b': numberNonBlank = 1; showLineNumbers = 1; break;
-					case 's': squeezeBlank = 1; break;
-					case 'E': showEnds = 1; break;
-					case 'T': showTabs = 1; break;
-					default:
-						fprintf(stderr, COLOR_BOLD_RED "cat: invalid option -- '%c'\n" COLOR_RESET, *opt);
-						printUsage();
-						return 1;
-				}
-				opt++;
-			}
-		} else {
-			firstFileIndex = i;
-			break;
-		}
-	}
-
-	if (firstFileIndex == argc) {
-		fprintf(stderr, COLOR_BOLD_RED "cat: missing file operand\n" COLOR_RESET);
-		printUsage();
-		return 1;
-	}
-
-	for (int i = firstFileIndex; i < argc; i++) {
-		printFile(argv[i]);
-	}
-
-	return 0;
+    return 0;
 }
--- src/commands/touch.c
+++ src/commands/touch.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <io.h>
 
-#define ANSI_BOLD_RED "\x1b[1;31m"
-#define ANSI_BOLD_GREEN "\x1b[1;32m"
+#define ANSI_BOLD_RED    "\x1b[1;31m"
+#define ANSI_BOLD_GREEN  "\x1b[1;32m"
 #define ANSI_BOLD_YELLOW "\x1b[1;33m"
-#define ANSI_RESET "\x1b[0m"
+#define ANSI_RESET       "\x1b[0m"
 
 void setConsoleUTF8() {
     SetConsoleOutputCP(CP_UTF8);
@@ -19,97 +18,28 @@ void setConsoleUTF8() {
     }
 }
 
-void printUsage() {
+void printHelp() {
     printf(ANSI_BOLD_YELLOW
-        "Usage: touch [OPTIONS] FILE...\n"
-        "Update the access and modification times of each FILE to the current time.\n\n"
+        "Usage: touch [file...]\n"
+        "\n"
+        "Create each file if it does not exist.\n"
+        "Update the access and modification times to the current time.\n"
+        "\n"
         "Options:\n"
-        "  -a          change only the access time\n"
-        "  -m          change only the modification time\n"
-        "  -c          do not create any files\n"
-        "  -t [[CC]YY]MMDDhhmm[.ss]\n"
-        "              use the specified timestamp instead of the current time\n"
-        "  --help      display this help and exit\n"
+        "  -h, --help      Show this help message.\n"
         ANSI_RESET);
 }
 
-int parseTime(const char* str, SYSTEMTIME* st) {
-    GetSystemTime(st);
-
-    int len = (int)strlen(str);
-    if (len < 8) return 0;
-
-    int pos = 0;
-    char buf[5];
-    memset(st, 0, sizeof(SYSTEMTIME));
-
-    if (len == 12 || len == 10 || len == 8 || len == 15) {
-        if (len == 15) {
-            strncpy(buf, str, 4);
-            buf[4] = 0;
-            int year = atoi(buf);
-            st->wYear = year;
-            pos = 4;
-        } else if (len == 12) {
-            strncpy(buf, str, 2);
-            buf[2] = 0;
-            int year = atoi(buf);
-            st->wYear = year + 2000;
-            pos = 2;
-        } else {
-            st->wYear = 2023;
-        }
-
-        strncpy(buf, str + pos, 2);
-        buf[2] = 0;
-        st->wMonth = atoi(buf);
-        pos += 2;
-
-        strncpy(buf, str + pos, 2);
-        buf[2] = 0;
-        st->wDay = atoi(buf);
-        pos += 2;
-
-        strncpy(buf, str + pos, 2);
-        buf[2] = 0;
-        st->wHour = atoi(buf);
-        pos += 2;
-
-        strncpy(buf, str + pos, 2);
-        buf[2] = 0;
-        st->wMinute = atoi(buf);
-        pos += 2;
-
-        if (pos + 3 <= len && str[pos] == '.') {
-            strncpy(buf, str + pos + 1, 2);
-            buf[2] = 0;
-            st->wSecond = atoi(buf);
-        } else {
-            st->wSecond = 0;
-        }
-
-        return 1;
-    }
-
-    return 0;
-}
-
-int touchFile(const char* filename, int changeAccess, int changeModify, int noCreate, SYSTEMTIME* customTime) {
+int touchFile(const char* filename) {
     int exists = (_access(filename, 0) == 0);
 
-    if (!exists && noCreate) {
-        printf(ANSI_BOLD_YELLOW "File does not exist and -c specified, skipping: %s\n" ANSI_RESET, filename);
-        return 0;
-    }
-
     if (!exists) {
-        HANDLE hFile = CreateFileA(filename, GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
-        if (hFile == INVALID_HANDLE_VALUE) {
+        HANDLE hNew = CreateFileA(filename, GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hNew == INVALID_HANDLE_VALUE) {
             printf(ANSI_BOLD_RED "Error creating file: %s\n" ANSI_RESET, filename);
             return 1;
         }
-        CloseHandle(hFile);
-        printf(ANSI_BOLD_GREEN "Created file: %s\n" ANSI_RESET, filename);
+        CloseHandle(hNew);
     }
 
     HANDLE hFile = CreateFileA(filename, FILE_WRITE_ATTRIBUTES, 0, NULL, OPEN_EXISTING, 0, NULL);
@@ -118,41 +48,19 @@ int touchFile(const char* filename, int changeAccess, int changeModify, int noCr
         return 1;
     }
 
-    FILETIME ftCreate, ftAccess, ftModify;
-    if (!GetFileTime(hFile, &ftCreate, &ftAccess, &ftModify)) {
-        printf(ANSI_BOLD_RED "Error getting file times: %s\n" ANSI_RESET, filename);
-        CloseHandle(hFile);
-        return 1;
-    }
+    SYSTEMTIME now;
+    FILETIME ft;
+    GetSystemTime(&now);
+    SystemTimeToFileTime(&now, &ft);
 
-    FILETIME ftNewAccess = ftAccess;
-    FILETIME ftNewModify = ftModify;
-
-    if (customTime) {
-        FILETIME ftCustom;
-        SystemTimeToFileTime(customTime, &ftCustom);
-        if (changeAccess) ftNewAccess = ftCustom;
-        if (changeModify) ftNewModify = ftCustom;
-    } else {
-        SYSTEMTIME stNow;
-        FILETIME ftNow;
-        GetSystemTime(&stNow);
-        SystemTimeToFileTime(&stNow, &ftNow);
-        if (changeAccess) ftNewAccess = ftNow;
-        if (changeModify) ftNewModify = ftNow;
-    }
-
-    if (!SetFileTime(hFile,
-                     NULL,
-                     changeAccess ? &ftNewAccess : &ftAccess,
-                     changeModify ? &ftNewModify : &ftModify)) {
-        printf(ANSI_BOLD_RED "Error setting file times: %s\n" ANSI_RESET, filename);
+    if (!SetFileTime(hFile, NULL, &ft, &ft)) {
+        printf(ANSI_BOLD_RED "Error setting file time: %s\n" ANSI_RESET, filename);
         CloseHandle(hFile);
         return 1;
     }
 
     CloseHandle(hFile);
-    printf(ANSI_BOLD_GREEN "Touched file: %s\n" ANSI_RESET, filename);
+    printf(ANSI_BOLD_GREEN "📝 Touched file: %s\n" ANSI_RESET, filename);
     return 0;
 }
 
@@ -160,58 +68,22 @@ int main(int argc, char* argv[]) {
     setConsoleUTF8();
 
     if (argc < 2) {
-        printUsage();
+        printf(ANSI_BOLD_RED "touch: missing file operand\n" ANSI_RESET);
+        printHelp();
         return 1;
     }
 
-    int changeAccess = 1;
-    int changeModify = 1;
-    int noCreate = 0;
-    SYSTEMTIME customTime;
-    SYSTEMTIME* pCustomTime = NULL;
-
-    int i = 1;
-    while (i < argc) {
-        if (strcmp(argv[i], "--help") == 0) {
-            printUsage();
-            return 0;
-        } else if (strcmp(argv[i], "-a") == 0) {
-            changeModify = 0;
-        } else if (strcmp(argv[i], "-m") == 0) {
-            changeAccess = 0;
-        } else if (strcmp(argv[i], "-c") == 0) {
-            noCreate = 1;
-        } else if (strcmp(argv[i], "-t") == 0) {
-            if (i + 1 >= argc) {
-                printf(ANSI_BOLD_RED "Option -t requires an argument.\n" ANSI_RESET);
-                return 1;
-            }
-            if (!parseTime(argv[i + 1], &customTime)) {
-                printf(ANSI_BOLD_RED "Invalid time format for -t.\n" ANSI_RESET);
-                return 1;
-            }
-            pCustomTime = &customTime;
-            i++;
-        } else if (argv[i][0] == '-') {
-            printf(ANSI_BOLD_RED "Unknown option: %s\n" ANSI_RESET, argv[i]);
-            return 1;
-        } else {
-            break;
-        }
-        i++;
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+        printHelp();
+        return 0;
     }
 
-    if (i >= argc) {
-        printf(ANSI_BOLD_RED "No files specified.\n" ANSI_RESET);
-        return 1;
-    }
-
-    int errorCount = 0;
-    for (; i < argc; i++) {
-        if (touchFile(argv[i], changeAccess, changeModify, noCreate, pCustomTime) != 0) {
-            errorCount++;
+    int errors = 0;
+    for (int i = 1; i < argc; i++) {
+        if (touchFile(argv[i]) != 0) {
+            errors++;
         }
     }
 
-    return errorCount == 0 ? 0 : 1;
+    return errors == 0 ? 0 : 1;
 }
