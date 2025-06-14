608da9a93b804dfe7a0f923d7195f29dc78a84f4
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-06-14 07:58:45 -0700

    add tr command

---
+++ src/commands/tr.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+#define BUFFER_SIZE 8192
+
+void remove_substring(const char *input, const char *sub, char *output) {
+    size_t sub_len = strlen(sub);
+    const char *p = input;
+    char *out = output;
+
+    while (*p) {
+        if (strncmp(p, sub, sub_len) == 0) {
+            p += sub_len;  
+        } else {
+            *out++ = *p++;
+        }
+    }
+    *out = '\0';
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <substring_to_remove>\n", argv[0]);
+        return 1;
+    }
+
+    const char *sub = argv[1];
+    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+
+    if (GetConsoleMode(hInput, &mode)) {
+        fprintf(stderr, "Error: Input must come from a pipe or file.\n");
+        return 1;
+    }
+
+    char buffer[BUFFER_SIZE];
+    char output[BUFFER_SIZE * 2]; 
+
+    DWORD bytesRead;
+    while (ReadFile(hInput, buffer, BUFFER_SIZE - 1, &bytesRead, NULL) && bytesRead > 0) {
+        buffer[bytesRead] = '\0';
+        remove_substring(buffer, sub, output);
+        DWORD bytesWritten;
+        WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), output, (DWORD)strlen(output), &bytesWritten, NULL);
+    }
+
+    return 0;
+}
