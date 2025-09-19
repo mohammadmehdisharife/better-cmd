769541196666e35a0d520b75a9e3846ddf4c333c
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-09-20 01:27:53 +0330

    fix error color on paste.c

--- src/commands/paste.c
+++ src/commands/paste.c
@@ -1,22 +1,23 @@
 #include <windows.h>
 #include <stdio.h>
+#include "../libs/ansi_colors.h"
 
 int main(void) {
     if (!OpenClipboard(NULL)) {
-        fprintf(stderr, "Cannot open clipboard.\n");
+        fprintf(stderr, ANSI_BOLD_RED "Cannot open clipboard.\n" ANSI_RESET);
         return 1;
     }
 
     HANDLE hData = GetClipboardData(CF_TEXT);
     if (hData == NULL) {
-        fprintf(stderr, "No text data in clipboard.\n");
+        fprintf(stderr, ANSI_BOLD_RED "No text data in clipboard.\n" ANSI_RESET);
         CloseClipboard();
         return 1;
     }
 
     char *pszText = (char*)GlobalLock(hData);
     if (pszText == NULL) {
-        fprintf(stderr, "Failed to lock clipboard data.\n");
+        fprintf(stderr, ANSI_BOLD_RED "Failed to lock clipboard data.\n" ANSI_RESET);
         CloseClipboard();
         return 1;
     }
