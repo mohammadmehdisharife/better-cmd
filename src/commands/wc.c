ce16a083aa94947cc27cee1b7e5ed2ac80dbbfca
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-09-20 12:20:18 +0330

    write man page agen

--- src/commands/wc.c
+++ src/commands/wc.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <windows.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
 #include "../libs/ansi_colors.h"
 
 int count_file(FILE *fp, unsigned long *lines, unsigned long *words, unsigned long *chars)
@@ -53,7 +56,7 @@ int main(int argc, char *argv[])
         fp = fopen(argv[1], "r");
         if (!fp)
         {
-            perror(ANSI_BOLD_RED "Error opening file" ANSI_RESET);
+            fprintf(stderr, ANSI_BOLD_RED "cat: cannot open '%s': %s\n" ANSI_RESET, argv[1], strerror(errno));
             return 1;
         }
     }
