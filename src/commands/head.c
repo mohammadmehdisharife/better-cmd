fbe7b927fdafe02af9adb23399e1f80d0a90afcb
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-09-20 23:57:16 +0330

    add pipe support to head.c

--- src/commands/head.c
+++ src/commands/head.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../libs/ansi_colors.h"
 
-void printHead(const char *filename, int lines) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        fprintf(stderr, ANSI_BOLD_RED "head: cannot open '%s'\n" ANSI_RESET, filename);
-        return;
-    }
+#define DEFAULT_LINES 10
+#define BUFFER_SIZE 1024
 
-    char buffer[4096];
+void print_head(FILE *fp, int nlines) {
+    char buffer[BUFFER_SIZE];
     int count = 0;
-    while (count < lines && fgets(buffer, sizeof(buffer), file)) {
-        fputs(buffer, stdout);
-        count++;
-    }
 
-    fclose(file);
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+        if (++count >= nlines)
+            break;
+    }
 }
 
 int main(int argc, char *argv[]) {
-    int lines = 10;
-    const char *filename = NULL;
+    int nlines = DEFAULT_LINES;
+    char *filename = NULL;
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-n") == 0 && i + 1 < argc) {
-            lines = atoi(argv[++i]);
-            if (lines < 0) lines = 10;
-        } else if (!filename) {
+            nlines = atoi(argv[++i]);
+        } else {
             filename = argv[i];
         }
     }
 
-    if (!filename) {
-        fprintf(stderr, ANSI_BOLD_RED "head: missing file operand\n" ANSI_RESET);
-        return 1;
+    if (filename != NULL) {
+        FILE *fp = fopen(filename, "r");
+        if (fp == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+        print_head(fp, nlines);
+        fclose(fp);
+    } else {
+        print_head(stdin, nlines);
     }
 
-    printHead(filename, lines);
     return 0;
 }
