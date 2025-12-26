dc83e8f1184118c0ebf04aae06aee7d6f01df22d
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-12-26 11:20:48 +0000

    fix bug on bettercmd script runer

--- src/commands/bettercmd.c
+++ src/commands/bettercmd.c
@@ -1,7 +1,20 @@
+#include <windows.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-  system("../bettercmd.bat");
-  return 0;
+int main() {
+    char exePath[MAX_PATH];
+    char batchPath[MAX_PATH];
+    char *lastBackslash;
+    
+    GetModuleFileNameA(NULL, exePath, MAX_PATH);
+    
+    lastBackslash = strrchr(exePath, '\\');
+    if (lastBackslash) *lastBackslash = '\0';
+    
+    sprintf(batchPath, "%s\\..\\conf\\bettercmd.bat", exePath);
+    
+    int result = system(batchPath);
+    
+    return result;
 }
