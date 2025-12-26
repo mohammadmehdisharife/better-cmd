1225b15325f16a78018f61c017095d66ca3cd2b1
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-12-26 16:48:11 +0000

    fix bug on bettercmd runer

--- src/commands/bettercmd.c
+++ src/commands/bettercmd.c
@@ -1,20 +1,44 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <shellapi.h>
 
 int main() {
     char exePath[MAX_PATH];
     char batchPath[MAX_PATH];
-    char *lastBackslash;
+    char resolvedPath[MAX_PATH];
     
     GetModuleFileNameA(NULL, exePath, MAX_PATH);
     
-    lastBackslash = strrchr(exePath, '\\');
-    if (lastBackslash) *lastBackslash = '\0';
+    char *lastBackslash = strrchr(exePath, '\\');
+    if (lastBackslash) {
+        *lastBackslash = '\0';
+    }
     
-    sprintf(batchPath, "%s\\..\\conf\\bettercmd.bat", exePath);
+    snprintf(batchPath, MAX_PATH, "%s\\..\\conf\\bettercmd.bat", exePath);
     
-    int result = system(batchPath);
+    if (GetFullPathNameA(batchPath, MAX_PATH, resolvedPath, NULL) == 0) {
+        MessageBoxA(NULL, "Failed to resolve batch file path", "Error", MB_ICONERROR);
+        return 1;
+    }
     
-    return result;
+    HINSTANCE hResult = ShellExecuteA(
+        NULL,           
+        "open",         
+        resolvedPath,   
+        NULL,           
+        NULL,           
+        SW_SHOWDEFAULT  
+    );
+    
+    if ((int)hResult <= 32) {
+        char errorMsg[256];
+        snprintf(errorMsg, sizeof(errorMsg), 
+                "Failed to execute batch file\nError code: %d", (int)hResult);
+        MessageBoxA(NULL, errorMsg, "Error", MB_ICONERROR);
+        return 1;
+    }
+    
+    return 0;
 }
